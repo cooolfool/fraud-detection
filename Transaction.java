933181beff5eb7407f69e7e043914afb00e024e3
cooolfool <131944015+cooolfool@users.noreply.github.com>
2025-03-26 23:46:42 +0530

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,23 @@
+package com.fraud.detection.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Data
+@Component
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userId;
+    private Double amount;
+    private String merchant;
+    private String location;
+    private boolean isFraud;
+}
