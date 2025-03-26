6e8d495751b80e4f173d6ee096708d92443c2ec9
cooolfool <131944015+cooolfool@users.noreply.github.com>
2025-03-26 23:45:38 +0530

    Create FraudTransactionDetectionController.java

---
+++ FraudTransactionDetectionController.java
@@ -0,0 +1,31 @@
+package com.fraud.detection.controller;
+
+import com.fraud.detection.entity.Transaction;
+import com.fraud.detection.service.FraudDetectionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class FraudTransactionDetectionController {
+
+    @Autowired
+    Transaction transaction;
+    @Autowired
+    FraudDetectionService fraudDetectionService;
+
+    @PostMapping("/isFraud")
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) throws IOException {
+        log.info("Request recieved in controller");
+        boolean isFraud = fraudDetectionService.isFraud(transaction);
+        transaction.setFraud(isFraud);
+        return ResponseEntity.ok(transaction);
+    }
+
+}
